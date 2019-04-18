9a2f56bfc0a8e7b38b2d50685730776629401eb8
l3inadz <espinoza.dev@gmail.com>
2019-04-18 10:45:31 -0300

    Exercises

---
+++ 2. Program Structure/chessboard/script.js
@@ -0,0 +1,21 @@
+startChessboard();
+
+function startChessboard() {
+  let board = '';
+  let size = Number(prompt('Enter board size'));
+  if (!Number.isNaN(size) && size) {
+    for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {                
+        if ((x + y) % 2 == 0) {
+          board += " ";
+        } else {
+          board += "#";
+        }
+      }
+      board += "\n";
+    }
+    console.log(board);
+  } else {
+    startChessboard();
+  }
+}
